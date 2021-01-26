4cc302af0d57154f4973b0b0e5d573659ee1290e
Jiibben <64984990+Jiibben@users.noreply.github.com>
2021-01-26 22:28:52 +0100

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,27 +1,30 @@
 import random as r
 from string import ascii_uppercase
-word = "test".upper()
-dotted_word = ["_" for i in word]
-max_tries = 3
-already_checked = list()
-tries = 0
-lost = False
+word = "test".upper() # selectionne le mot que le jouer doit trouver (et le change en majuscule)
+dotted_word = ["_" for i in word] # crée un mot en underscore (_ _ _ _ ) en se basant sur la variable word
+max_tries = 3 #essais maximum avant d'être "pendu"
+already_checked = list() #crée une liste des lettres deja utilisée
+tries = 0 #essais déjà fais
 
+# retourne une liste des indexes ou la lettre (char) à été trouvée dans (word)
 def find_indexes(char, word):
     if char in word:
         return [i for i in range(0, len(word)) if word[i] == char]
     else:
         return None
 
+# change les underscores du mot en lettre selon le charactère donné
 def update_dottedword(word, char, dotted_word):
     for i in find_indexes(char, word):
         dotted_word[i] = char
         already_checked.append(char)
     return dotted_word
 
+# affiche le mot de manière jolie (change la liste en string)
 def output(dotted_word):
     return " ".join(dotted_word)
 
+# utile pour voir si le joueur à gagner
 def check_win(dotted_word, word):
     if dotted_word == list(word):
         return True
@@ -31,7 +34,7 @@ def check_win(dotted_word, word):
 #game loop
 while True:
     guess = input("entre une lettre: ").upper()
-    if len(guess) > 1 or guess not in ascii_uppercase or guess  in already_checked:
+    if len(guess) > 1 or guess not in ascii_uppercase or guess in already_checked:
         print("tu dois entrer une seule lettre et qui n'a jamais été utilisée")
         continue
     if guess in word:
